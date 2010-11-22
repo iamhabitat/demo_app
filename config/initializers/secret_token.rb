e0fd932c924a434ef88c6a9a864c1e92d8fc51ed
Ben Myers <ben@ben-myers.com>
2010-11-22 06:46:25 +0530

    Initial commit sucka

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '07e6dc6d9530cd99dc0d92c09d429c052e85d5c058569ae8e16bcd2be8bdbed4b85dfe2056ff64f6a8d74a034eb83afd23bba5e4b45eef3e9115aa3eca46f48b'
